d90ac5b55dbfd29d56f6548c43ecbba90b49f321
Unknown <samar.gamal144@gmail.com>
2018-06-09 17:45:46 +0200

    commit

--- src/main/java/API/UserController.java
+++ src/main/java/API/UserController.java
@@ -1,6 +1,4 @@
 package API;
-import API.LocationData.Location;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
